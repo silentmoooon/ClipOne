02145a9aca91aa75d1637f9c0404441a86a1a98c
silentmoon <xie.can@outlook.com>
2019-05-13 13:02:55 +0800

    添加Images字段

--- ClipOne/model/ClipModel.cs
+++ ClipOne/model/ClipModel.cs
@@ -17,6 +17,12 @@
         /// </summary>
         public string DisplayValue { get; set; }
 
+
+        /// <summary>
+        /// 涉及到的图片资源,方便清除图片,针对QQ类型
+        /// </summary>
+        public string Images;
+
       
         
     }
