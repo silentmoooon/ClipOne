8b92eead1dae592161897945948e0ea556c55cc4
vessel <xie.can@outlook.com>
2019-12-01 22:16:45 +0800

    .net core 3.0

---
+++ ClipOneCore/util/LimitPropsContractResolver.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClipOne.util
+{
+    public class LimitPropsContractResolver : DefaultContractResolver
+    {
+        string[] props = null;
+ 
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="props">传入的属性数组</param>
+        /// <param name="retain">true:表示props是需要保留的字段  false：表示props是要排除的字段</param>
+        public LimitPropsContractResolver(string[] props)
+        {
+            //指定要序列化属性的清单
+            this.props = props;
+
+            
+        }
+
+        protected override IList<JsonProperty> CreateProperties(Type type,
+
+        MemberSerialization memberSerialization)
+        {
+            IList<JsonProperty> list =
+            base.CreateProperties(type, memberSerialization);
+            //只保留清单有列出的属性
+            return list.Where(p =>
+            {
+                return props.Contains(p.PropertyName);
+
+            }).ToList();
+        }
+    }
+}
