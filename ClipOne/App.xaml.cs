a99cde80d06424173595d0a0e8c1295d91fcb812
vessel <xie.can@outlook.com>
2019-12-02 12:09:28 +0800

    修改路径和工程名

---
+++ ClipOne/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ClipOne.model;
+using ClipOne.service;
+using ClipOne.util;
+using ClipOne.view;
+using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using System.Windows.Threading;
+
+namespace ClipOne
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        
+        void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+             
+             
+            e.Handled = true;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+              base.OnStartup(e);
+            Process[] pro = Process.GetProcesses();
+
+            int n = pro.Where(p => p.ProcessName.ToLower().Equals(Process.GetCurrentProcess().MainModule.ModuleName.ToLower())).Count();
+            if (n > 1)
+            {
+
+                Current.Shutdown();
+                return;
+            }
+
+
+        }
+
+        
+
+    }
+
+}
---
+++ ClipOne/util/HotKeyManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ClipOne.util
+{
+    /// <summary>
+    /// 热键管理器
+    /// </summary>
+    public class HotKeyManager
+    {
+        /// <summary>
+        /// 热键消息
+        /// </summary>
+        public const int WM_HOTKEY = 0x312;
+
+        /// <summary>
+        /// 注册热键
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifuers, int vk);
+
+        /// <summary>
+        /// 注销热键
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        /// <summary>
+        /// 向原子表中添加全局原子
+        /// </summary>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern short GlobalAddAtom(string lpString);
+
+        /// <summary>
+        /// 在表中搜索全局原子
+        /// </summary>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern short GlobalFindAtom(string lpString);
+
+        /// <summary>
+        /// 在表中删除全局原子
+        /// </summary>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern short GlobalDeleteAtom(string nAtom);
+
+        /// <summary>
+        /// 定义了辅助键的名称（将数字转变为字符以便于记忆，也可去除此枚举而直接使用数值）
+        /// </summary>
+        [Flags()]
+        public enum KeyModifiers
+        {
+            None = 0,
+            Alt = 1,
+            Ctrl = 2,
+            Shift = 4,
+            WindowsKey = 8
+        }
+        /// <summary>
+        /// 热键的对应的消息ID
+        /// </summary>
+        public const int WmHotkey = 0x312;
+    }
+}
